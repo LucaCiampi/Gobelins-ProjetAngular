a6a8387470721203614734c19f272b44ed892eb2
LucaCiampi <luca.ciampi@edu.gobelins.fr>
2021-11-13 11:45:43 +0100

    layout

--- src/app/components/image-view/image-view.component.ts
+++ src/app/components/image-view/image-view.component.ts
@@ -1,52 +1,79 @@
 import { Component, OnInit } from '@angular/core';
-import {ImageModel} from "../../models/image.model";
-import * as faker from "faker";
+import { ImageModel } from '../../models/image.model';
+import * as faker from 'faker';
 
 @Component({
   selector: 'app-image-view',
   templateUrl: './image-view.component.html',
-  styleUrls: ['./image-view.component.css']
+  styleUrls: ['./image-view.component.css'],
 })
 export class ImageViewComponent implements OnInit {
-
   imageList: ImageModel[];
 
   constructor() {
     this.imageList = [
       new ImageModel({
-        imageUrl: faker.image.imageUrl(140, 140, undefined, true, true),
-        title: faker.name.firstName(),
-        text: faker.lorem.sentence(12)
+        imageUrl: "../assets/img/placeholder-500x500.jpg",
+        title: 'This is my title',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
       }),
       new ImageModel({
-        imageUrl: faker.image.imageUrl(140, 140, undefined, true, true),
-        title: faker.name.firstName(),
-        text: faker.lorem.sentence(12)
+        imageUrl: "../assets/img/placeholder-500x500.jpg",
+        title: 'This is my title',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
       }),
       new ImageModel({
-        imageUrl: faker.image.imageUrl(140, 140, undefined, true, true),
-        title: faker.name.firstName(),
-        text: faker.lorem.sentence(12)
+        imageUrl: "../assets/img/placeholder-500x500.jpg",
+        title: 'This is my title',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
       }),
       new ImageModel({
-        imageUrl: faker.image.imageUrl(140, 140, undefined, true, true),
-        title: faker.name.firstName(),
-        text: faker.lorem.sentence(12)
+        imageUrl: "../assets/img/placeholder-500x500.jpg",
+        title: 'This is my title',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
       }),
       new ImageModel({
-        imageUrl: faker.image.imageUrl(140, 140, undefined, true, true),
-        title: faker.name.firstName(),
-        text: faker.lorem.sentence(12)
+        imageUrl: "../assets/img/placeholder-500x500.jpg",
+        title: 'This is my title',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
       }),
       new ImageModel({
-        imageUrl: faker.image.imageUrl(140, 140, undefined, true, true),
-        title: faker.name.firstName(),
-        text: faker.lorem.sentence(12)
+        imageUrl: "../assets/img/placeholder-500x500.jpg",
+        title: 'This is my title',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
       })
-    ]
+      // new ImageModel({
+      //   imageUrl: faker.image.imageUrl(320, 320, undefined, true, true),
+      //   title: faker.name.firstName(),
+      //   text: faker.lorem.sentence(12)
+      // }),
+      // new ImageModel({
+      //   imageUrl: faker.image.imageUrl(320, 320, undefined, true, true),
+      //   title: faker.name.firstName(),
+      //   text: faker.lorem.sentence(12)
+      // }),
+      // new ImageModel({
+      //   imageUrl: faker.image.imageUrl(320, 320, undefined, true, true),
+      //   title: faker.name.firstName(),
+      //   text: faker.lorem.sentence(12)
+      // }),
+      // new ImageModel({
+      //   imageUrl: faker.image.imageUrl(320, 320, undefined, true, true),
+      //   title: faker.name.firstName(),
+      //   text: faker.lorem.sentence(12)
+      // }),
+      // new ImageModel({
+      //   imageUrl: faker.image.imageUrl(320, 320, undefined, true, true),
+      //   title: faker.name.firstName(),
+      //   text: faker.lorem.sentence(12)
+      // }),
+      // new ImageModel({
+      //   imageUrl: faker.image.imageUrl(320, 320, undefined, true, true),
+      //   title: faker.name.firstName(),
+      //   text: faker.lorem.sentence(12)
+      // })
+    ];
   }
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
